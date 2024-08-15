0db6d8de13efc7d4748839c56d6e18b85870f898
nlh <nlhsueh@gmail.com>
2024-08-15 17:15:25 +0800

    init

--- exercise/olympic/src/components/medals.vue
+++ exercise/olympic/src/components/medals.vue
@@ -1,73 +1,98 @@
 
 <template>
-    <div class="row justify-content-center" id="medalTable">
-        <div class="country-card col-lg-2 col-md-3 col-sm-4 col-6" v-for="country in countryList">
-            <div class="card-body">
-                <h5 class="card-title"> {{country.name}}</h5>
-                <img :src="'/img/' + country.img" class="card-img-top">
-                <div class="medals mt-2">
-                    <span class="medal gold">{{country.gold}}</span>
-                    <span class="medal silver">{{country.silver}}</span>
-                    <span class="medal bronze">{{country.bronze}}</span>
-                </div>
+    <div class="container mt-3">
+        <sort @sortByName="sortByName" @sortByGold="sortByGold" @sortByTotal="sortByTotal" />
+
+        <div class="row justify-content-center" id="medalTable">
+            <div v-for="country in countryList" :key="country.name" class="country-card col-lg-2 col-md-3 col-sm-4 col-6">
+                <c_medal :countryName="country.name" :countryImg="country.img" :gold="country.gold" :silver="country.silver"
+                    :bronze="country.bronze" />
             </div>
         </div>
+
     </div>
 </template>
-
+  
 <script>
-    export default {
-        props: ['countryList'],
-    }        
+
+export default {
+    data() {
+        return {
+            medals: null,
+            img_url: {
+                Australia: "australia.png",
+                Azerbaijan: "azerbaijan.png",
+                Belgium: "belgium.png",
+                Brazil: "brazil.png",
+                Canada: "canada.png",
+                China: "china.png",
+                France: "france.png",
+                Germany: "germany.png",
+                Great_Britain: "great-britain.png",
+                Hong_Kong: "hong-kong.png",
+                India: "india.png",
+                Italy: "italy.png",
+                Japan: "japan.png",
+                Kazakhstan: "kazakhstan.png",
+                Moldova: "moldova.png",
+                Republic_of_Korea: "skorea.png",
+                South_Africa: "south-africa.png",
+                Sweden: "sweden.png",
+                Turkey: "turkey.png",
+                United_States: "united-states.png"
+            },
+            countryList: [
+            ],
+            selectedCountry: "",
+            goldMedals: 0,
+            silverMedals: 0,
+            bronzeMedals: 0,
+        };
+    },
+    async created() {
+        await this.fetch_medals();
+        this.build_country_list();
+        console.log(this.countryList);
+    },
+    methods: {
+        async fetch_medals() {
+            const response = await fetch("medals.json");
+            const data = await response.json();
+            this.medals = data.medals;
+        },
+        build_country_list() {
+            if (!this.medals) {
+                console.log('Medals is not built');
+                return;
+            }
+            this.countryList = this.medals.map(medal => {
+                const [name, gold, silver, bronze] = medal.split(' ');
+                return {
+                    name: name.replace(/-/g, ' '),
+                    gold: parseInt(gold),
+                    silver: parseInt(silver),
+                    bronze: parseInt(bronze),
+                    total: parseInt(gold) + parseInt(silver) + parseInt(bronze),
+                    img: this.img_url[name.replaceAll('-', '_')],
+                };
+            });
+            console.log('country list: ', this.countryList);
+        },
+        sortByName() {
+            this.countryList.sort((a, b) => a.name.localeCompare(b.name))
+        },
+        sortByGold() {
+            this.countryList.sort((a, b) => b.gold - a.gold);
+        },
+        sortByTotal() {
+            this.countryList.sort((a, b) => b.total - a.total);
+        },
+        updateMedals(countryList) {
+            this.countryList = countryList;
+        }
+    }
+}
 </script>
 
-<style>  
-    .country-card {
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        border: 1px solid #ddd;
-        border-radius: 8px;
-        height: 300px;
-        margin: 10px;
-        padding: 10px;
-    }
-
-    .card-img-top {
-        width: 100px;
-        height: auto;
-        margin-bottom: 10px;
-    }
-
-    .card-body {
-        text-align: center;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-    }
-
-    .medal {
-        display: inline-block;
-        width: 40px;
-        height: 40px;
-        line-height: 40px;
-        border-radius: 4px;
-        margin: 0 5px;
-        color: black;
-        font-size: 1.2em;
-    }
-
-    .gold {
-        background-color: #FFD700;
-    }
-
-    .silver {
-        background-color: #C0C0C0;
-    }
-
-    .bronze {
-        background-color: #CD7F32;
-    }
-</style>    
+  
+<style></style>  
